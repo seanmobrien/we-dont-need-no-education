761b6e4da04f0a8c593bdd5f698da948b03455b9
seanmobrien <seanmobrien@hotmail.com>
2025-06-09 14:30:32 -0500

    Update _client.ts

--- web-ui/lib/site-util/env/_client.ts
+++ web-ui/lib/site-util/env/_client.ts
@@ -34,7 +34,7 @@ export const clientRawInstance = {
     process.env.NEXT_PUBLIC_LOG_LEVEL_CLIENT ?? 'silly',
   /**
    * The cache timeout for client-side data grids.
-   * @type {string | undefined}
+   * @type {number | undefined}
    */
   NEXT_PUBLIC_DATAGRID_CLIENT_CACHE_TIMEOUT:
     process.env.NEXT_PUBLIC_DATAGRID_CLIENT_CACHE_TIMEOUT ?? (5 * 60 * 1000),
