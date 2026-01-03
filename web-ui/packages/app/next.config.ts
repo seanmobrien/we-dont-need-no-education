6d5f2e21ffc6d8048c44dc44d183dc2a149c0cee
Sean <seanmobrien@hotmail.com>
2026-01-03 16:21:35 -0600

    hotfix: container

--- web-ui/packages/app/next.config.ts
+++ web-ui/packages/app/next.config.ts
@@ -4,14 +4,21 @@ import { withIgnorePacks } from './lib/config/ignore-unsupported-packs-plugin';
 import { withStripRscPrefixPlugin } from './lib/config/strip-rsc-prefix-plugin';
 import { withReactConfigFactory, withTypescriptConfig } from './lib/config';
 
+let isForStandalone = false;
+if (process && process.env && process.env.FOR_STANDALONE == '1') {
+  isForStandalone = true;
+}
+
+console.log(`Next.js Config - FOR_STANDALONE:${process.env.FOR_STANDALONE} evaluates as ${isForStandalone}`);
+
 export const nextConfig: NextConfig = withStripRscPrefixPlugin(
   withIgnorePacks(
     withBundleAnalyzer(
       withReactConfigFactory()(
         withTypescriptConfig({
-          ...(process.env.FOR_STANDALONE == '1'
+          ...(isForStandalone
             ? { output: 'standalone' }
-            : {}),
+            : { output: 'standalone' }),
           experimental: {
             webVitalsAttribution: ['CLS', 'LCP', 'FCP', 'INP', 'TTFB', 'FID'],
           },
